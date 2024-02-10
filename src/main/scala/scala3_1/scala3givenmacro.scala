8f1bdb884661334a77906959f7433481dc2de420
ValentinShilin <valentin.shilin.de@gmail.com>
2024-02-10 20:20:58 +0100

    scala3

--- src/main/scala/scala3_1/scala3givenmacro.scala
+++ src/main/scala/scala3_1/scala3givenmacro.scala
@@ -1,19 +1,1 @@
 package scala3_1
-
-object  scala3macros {
-  trait Show[T]{
-    inline def show(x:T): String
-  }
-
-  case class Foo(x:Int)
-
-  inline given Show[Foo] with {
-    inline def show(x:Foo): String = s"11 ${x.toString} 111"
-  }
-
-
-
-  @main def scala3macrosEx()={
-    println(summon[Show[Foo]].show(Foo(42)))
-  }
-}
