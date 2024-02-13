c60d09466aecedb6a0ae5827bb19162d72682c12
ValentinShilin <valentin.shilin.de@gmail.com>
2024-02-13 19:20:41 +0100

    scala3

--- src/main/scala/scala3_1/scala3newconstructions.scala
+++ src/main/scala/scala3_1/scala3newconstructions.scala
@@ -1,1 +1,38 @@
 package scala3_1
+
+object scala3testnewconstr {
+  def f(x:Int): Int = {
+    x*2
+  }
+
+  def f_withbug():Int = {
+    4/0
+  }
+
+
+  @main def test(): Unit = {
+    var x = 4
+    val xs = 1 :: 2 :: 2 :: 3 ::4 :: Nil
+    val ys = 4 :: 5 :: 6 :: Nil
+    if x<0 then
+      "sdf"
+    else if x == 0 then
+      "sdfs"
+    else
+      "sdf"
+
+    while x>0 do x = f(x)
+    val x1 = if x >5 then "sdf" else 1
+
+    for
+      x <- xs
+      y <- ys
+    do
+      println(x+y)
+
+    try f_withbug()
+    catch  case ex: Exception => println("error")
+
+
+  }
+}
