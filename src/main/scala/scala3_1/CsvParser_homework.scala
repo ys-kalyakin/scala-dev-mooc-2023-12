a215831b11bd921afb600112a74939adba2103d1
ys.kalyakin <ys.kalyakin@gmail.com>
2024-02-21 23:08:48 +0300

    scala 3.1

--- src/main/scala/scala3_1/CsvParser_homework.scala
+++ src/main/scala/scala3_1/CsvParser_homework.scala
@@ -5,7 +5,6 @@ package scala3_1
 
 
 
-/*
 //1. исполользовать given, как написано в комментариях и в почеченных местах ниже
 //2. использовать новый "тихий синтаксис", где сочтете приемлемым, тут на ваше усмотрение
 //https://docs.scala-lang.org/scala3/new-in-scala3.html  глава New & Shiny: The Syntax
@@ -13,76 +12,82 @@ package scala3_1
 
 
 class MonadParser[T, Src](private val p: Src => (T, Src)) {
-  def flatMap[M](f: T => MonadParser[M, Src]): MonadParser[M, Src] =
-    MonadParser { src =>
-      val (word, rest) = p(src)
-      val mn = f(word)
-      val res = mn.p(rest)
+    def flatMap[M](f: T => MonadParser[M, Src]): MonadParser[M, Src] =
+        MonadParser { src =>
+            val (word, rest) = p(src)
+            val mn = f(word)
+            val res = mn.p(rest)
 
-      //с помощью функции — аргумента метода добавляем его в контекст, видимый всем последующим парсерам по цепочке.
-      res
-    }
-  def map[M](f: T => M): MonadParser[M, Src] =
-    MonadParser { src =>
-      val (word, rest) = p(src)
-      (f(word), rest)
-    }
-  def parse(src: Src): T = p(src)._1
+            //с помощью функции — аргумента метода добавляем его в контекст, видимый всем последующим парсерам по цепочке.
+            res
+        }
+    def map[M](f: T => M): MonadParser[M, Src] =
+        MonadParser { src =>
+            val (word, rest) = p(src)
+            (f(word), rest)
+        }
+    def parse(src: Src): T = p(src)._1
 }
 
 object MonadParser {
-  def apply[T, Src](f: Src => (T, Src)) =
-    new MonadParser[T, Src](f)
+    def apply[T, Src](f: Src => (T, Src)) =
+        new MonadParser[T, Src](f)
 }
 
 trait FieldConversion[A,B]:
-  def convert(x: A): B
+    def convert(x: A): B
 
 given intFieldConversion: FieldConversion[String,Int] with
-  def convert(x: String): Int = ???
-// сделать given instance для типов Int Float Double
-// в функции просто сконвертнуть строку в нужный тип
+    def convert(x: String): Int = x.toInt
+
+given floatFieldConversion: FieldConversion[String, Float] with
+    def convert(x: String): Float = x.toFloat
+
+given doubleFieldConversion: FieldConversion[String, Double] with
+    def convert(x: String): Double = x.toDouble
+
+given booleanFieldConversion: FieldConversion[String, Boolean] with
+    def convert(x: String): Boolean = x.toBoolean
 
 object TestExecution{
 
-  //здесь написать функцию, которая будет применять given определенные выше
-  // использовать using fieldConversion c первым параметром String, а второй будет вариативны параметр B
+    //здесь написать функцию, которая будет применять given определенные выше
+    // использовать using fieldConversion c первым параметром String, а второй будет вариативны параметр B
 
-  def parse[String,B](x:String)(?????????) : B =
-    ...вызвать собственнь функцию из трейта FieldConversion...
+    def parse[String,B](x:String)(using converter: FieldConversion[String, B]) : B =
+        converter.convert(x)
+
+    def main(args: Array[String]): Unit = {
+
+        def StringField =
+            MonadParser[String, String] { str =>
+                val idx = str.indexOf(";")
+                if (idx > -1)
+                    (str.substring(0, idx), str.substring(idx + 1))
+                else
+                    (str, "")
+            }
+
+        def IntField: MonadParser[Int, String] = StringField.map(x => parse(x))
+        def FloatField: MonadParser[Float, String] = StringField.map(x => summon[FieldConversion[String, Float]].convert(x))
+        def BooleanField: MonadParser[Boolean, String] = StringField.map(x => summon[FieldConversion[String, Boolean]].convert(x))
+
+        case class Car(year: Int, mark: String, model: String, comment: String, price: Float)
+
+        val str = "1997;Ford;E350;ac, abs, moon;3000\n1996; Jeep; Grand Cherokee; MUST SELL! air, moon roof, loaded; 4799"
+
+        val parser =
+            for {
+                year <- IntField
+                mark <- StringField
+                model <- StringField
+                comment <- StringField
+                price <- FloatField
+            } yield Car(year, mark, model, comment, price)
 
 
-  def main(args: Array[String]): Unit = {
+        val result = str.split('\n').map(parser.parse)
 
-    def StringField =
-      MonadParser[String, String] { str =>
-        val idx = str.indexOf(";")
-        if (idx > -1)
-          (str.substring(0, idx), str.substring(idx + 1))
-        else
-          (str, "")
-      }
-
-    def IntField =  ??? //StringField.map(...здесь применить parse который подхватит нужный given автоматически ...)
-    def FloatField = ???
-    def BooleanField =???
-
-    case class Car(year: Int, mark: String, model: String, comment: String, price: Float)
-
-    val str = "1997;Ford;E350;ac, abs, moon;3000\n1996; Jeep; Grand Cherokee; MUST SELL! air, moon roof, loaded; 4799"
-
-    val parser =
-      for {
-        year <- IntField
-        mark <- StringField
-        model <- StringField
-        comment <- StringField
-        price <- FloatField
-      } yield Car(year, mark, model, comment, price)
-
-
-    val result = str.split('\n').map(parser.parse)
-
-    println(result.map(x=>s"${x.model},${x.mark},${x.year}").mkString(";"))
-  }
-}*/
+        println(result.map(x=>s"${x.model},${x.mark},${x.year}").mkString(";"))
+    }
+}
